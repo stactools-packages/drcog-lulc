fb94750c01679247290ea364c1b51db73fdf4768
pjhartzell <preston.hartzell@gmail.com>
2022-07-01 12:36:50 -0400

    add tiler

--- src/stactools/drcog_lulc/commands.py
+++ src/stactools/drcog_lulc/commands.py
@@ -1,9 +1,13 @@
 import logging
+import os
+from typing import Optional
 
 import click
 from pystac import CatalogType
 
 from stactools.drcog_lulc import stac
+from stactools.drcog_lulc.constants import DEFAULT_TILE_SIZE
+from stactools.drcog_lulc.tile import remove_nodata, tile
 
 logger = logging.getLogger(__name__)
 
@@ -66,5 +70,65 @@ def create_drcog_lulc_command(cli):
         item.save_object(dest_href=destination)
 
         return None
+
+    @drcog_lulc.command("tile", help="Tiles a GeoTIFF to a grid of COGs")
+    @click.argument("INFILE")
+    @click.argument("OUTDIR")
+    @click.argument("ORIGIN_X", type=int)
+    @click.argument("ORIGIN_Y", type=int)
+    @click.option(
+        "-s", "--size", type=int, default=DEFAULT_TILE_SIZE, help="Tile size in meters"
+    )
+    @click.option("-n", "--nodata", type=int, help="nodata value")
+    def tile_command(
+        infile: str,
+        outdir: str,
+        origin_x: int,
+        origin_y: int,
+        size: int,
+        nodata: Optional[int] = None,
+    ) -> None:
+        """Tiles an input GeoTIFF to a grid of COGs.
+
+        \b
+        Args:
+            infile (str): HREF to source GeoTIFF to be tiled
+            outdir (str): Directory that will contain the COG tiles
+            origin_x (int): X coordinate of the tile grid origin. The origin is
+                the lower left corner of the area to be tiled.
+            origin_y (int): Y coordinate of the tile grid origin. The origin is
+                the lower left corner of the area to be tiled.
+            size (int, optional): Tile size in meters. Default is 10000.
+            nodata (int, optional): nodata value to use for tiled COGs. Only
+                necessary if the INFILE GeoTIFF does not contain a correct
+                nodata value.
+        """
+        tile(infile, outdir, size, origin_x, origin_y, nodata)
+
+    @drcog_lulc.command(
+        "remove-nodata-tifs", help="Removes TIF files that contain only nodata values"
+    )
+    @click.argument("INDIR")
+    @click.option("-n", "--nodata_dir", help="directory to place nodata TIF files")
+    def remove_nodata_tifs_command(
+        indir: str, nodata_dir: Optional[str] = None
+    ) -> None:
+        """Moves TIF files that contain only nodata values to a new directory.
+
+        Useful after tiling a large area where many tiles do not intersect valid
+        data. The default location for the nodata-only TIF files is a new
+        subdirectory named 'nodata_tifs' within the directory of the TIF files
+        being examined. Use the --nodata_dir option to override this default
+        name and location.
+
+        \b
+        Args:
+            indir (str): Directory of TIF files to be examined.
+            nodata_dir (Optional[str]): Optional directory for the nodata TIF
+                files.
+        """
+        if nodata_dir is None:
+            nodata_dir = os.path.join(indir, "nodata_tifs")
+        remove_nodata(indir, nodata_dir)
 
     return drcog_lulc
---
+++ src/stactools/drcog_lulc/tile.py
@@ -0,0 +1,115 @@
+import os
+import shutil
+from glob import glob
+from typing import List, Optional
+
+import numpy as np
+import rasterio
+from stactools.core.utils.subprocess import call
+
+
+class Tile:
+    def __init__(
+        self,
+        left: float,
+        bottom: float,
+        right: float,
+        top: float,
+        nodata: Optional[int] = None,
+    ):
+        self._left = left
+        self._bottom = bottom
+        self._right = right
+        self._top = top
+        self._nodata = nodata
+
+    def subset(self, infile, outdir) -> None:
+        base = os.path.splitext(os.path.basename(infile))[0]
+        outfile = os.path.join(
+            outdir, (f"{base}_E{str(int(self._left))}_N{str(int(self._bottom))}.tif")
+        )
+        args = [
+            "gdal_translate",
+            "-of",
+            "COG",
+            "-co",
+            "compress=deflate",
+            "-co",
+            "blocksize=512",
+            "-projwin",
+            str(self._left),
+            str(self._top),
+            str(self._right),
+            str(self._bottom),
+        ]
+        if self._nodata is not None:
+            args.extend(["-a_nodata", str(self._nodata)])
+        args.append(infile)
+        args.append(outfile)
+        call(args)
+
+
+def tile(
+    infile: str,
+    outdir: str,
+    size: int,
+    left: int,
+    bottom: int,
+    nodata: Optional[int] = None,
+) -> None:
+    """Tiles the given input to a grid."""
+    with rasterio.open(infile) as dataset:
+        _, _, right, top = dataset.bounds
+        tiles = _create_tiles(left, bottom, right, top, size, nodata)
+    for tile in tiles:
+        tile.subset(infile, outdir)
+
+
+def _create_tiles(
+    left: float,
+    bottom: float,
+    right: float,
+    top: float,
+    size: int,
+    nodata: Optional[int] = None,
+) -> List[Tile]:
+    x = left
+    y = bottom
+    tiles = []
+    while x < right:
+        while y < top:
+            tile = Tile(x, y, min(x + size, right), min(y + size, top), nodata)
+            tiles.append(tile)
+            y += size
+        x += size
+        y = bottom
+    return tiles
+
+
+def remove_nodata(indir: str, nodata_dir: str) -> None:
+    """Removes TIF files that contain only nodata values to a new directory.
+
+    Args:
+        indir (str): Directory containing the TIF files.
+        nodata_dir (str): New directory for the TIF files that contain only
+            nodata values.
+    """
+    os.mkdir(nodata_dir)
+
+    tif_files = glob(f"{indir}/*.tif")
+    for tif_file in tif_files:
+        all_nodata = False
+        with rasterio.open(tif_file) as src:
+            nodata = src.nodata
+            data = src.read(1)
+            if np.all((data == nodata)):
+                all_nodata = True
+
+        if all_nodata:
+            filename = os.path.basename(tif_file)
+            shutil.move(tif_file, os.path.join(nodata_dir, filename))
+            print("R ", end="", flush=True)
+        else:
+            print(". ", end="", flush=True)
+
+    print()
