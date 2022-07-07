06f44b73d4a5c668ba8965ad03c32278cfd453ac
pjhartzell <preston.hartzell@gmail.com>
2022-07-07 17:20:13 -0400

    add media types to links

--- src/stactools/drcog_lulc/constants.py
+++ src/stactools/drcog_lulc/constants.py
@@ -57,7 +57,10 @@ COLLECTION_TITLE = "DRCOG LULC"
 COLLECTION_DESCRIPTION = "The Denver Regional Council of Governments (DRCOG) Land Use/Land Cover (LULC) datasets are developed in partnership with Babbit Center for Land and Water Policy and the Chesapeake Conservancy's Conservation Innovation Center (CIC). DRCOG LULC includes 2018 data at 1m (3.28ft) resolution covering 1,000 square miles and 2020 data at 1ft resolution covering 6,000 square miles of the Denver, Colorado region. The data is based on USDA's 1 meter National Agricultural Imagery Program (NAIP) aerial imagery and leaf-off aerial ortho-imagery captured as part of the Denver Regional Aerial Photography Project (6-inch resolution everywhere except the mountainous regions to the west, which came in 1-foot resolution)."  # noqa
 LICENSE = "proprietary"
 LICENSE_LINK = Link(
-    rel="license", target="https://drcog.org/legal-terms", title="DRCOG Legal Terms"
+    rel="license",
+    target="https://drcog.org/legal-terms",
+    title="DRCOG Legal Terms",
+    media_type="text/html",
 )
 KEYWORDS = ["Land Cover", "Land Use", "NAIP", "USDA"]
 PROVIDERS = [
@@ -98,9 +101,11 @@ REPORT_LINK = Link(
     rel="describedby",
     target="https://gis.drcog.org/rdc/supplemental/lulc_pilot_report.zip",
     title="Supplemental Information",
+    media_type="application/zip",
 )
 DATA_LINK = Link(
     rel="original",
     target="https://www.dropbox.com/s/0v2g1nqigm8604h/DRCOG_Final_Classification.zip?dl=0",
     title="DRCOG_Final_Classification",
+    media_type="application/zip",
 )
