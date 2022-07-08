b5aba2e3800770c904eade5dea0114262c5304de
Paola Holleway <paolaholleway@gmail.com>
2022-07-08 10:50:13 -0500

    ./scripts/format

--- src/stactools/drcog_lulc/constants.py
+++ src/stactools/drcog_lulc/constants.py
@@ -105,14 +105,14 @@ REPORT_LINK = Link(
 )
 DATA_LINK = [
     Link(
-    rel="original",
-    target="https://landcoverarchive.s3.amazonaws.com/2018/lulc_pilot_raster_2018.zip",
-    title="Land_Cover_Raster_Data_2018",
-    media_type="application/zip",
+        rel="original",
+        target="https://landcoverarchive.s3.amazonaws.com/2018/lulc_pilot_raster_2018.zip",
+        title="Land_Cover_Raster_Data_2018",
+        media_type="application/zip",
     ),
     Link(
-    target="https://landcoverarchive.s3.amazonaws.com/2020/DRCOG_2020_Landcover.zip",
-    title="Land_Cover_Raster_Data_2020",
-    media_type="application/zip",
+        target="https://landcoverarchive.s3.amazonaws.com/2020/DRCOG_2020_Landcover.zip",
+        title="Land_Cover_Raster_Data_2020",
+        media_type="application/zip",
     ),
 ]
